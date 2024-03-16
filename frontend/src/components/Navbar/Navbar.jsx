aa4720a30d68cb3556003197fdee721a3a99b911
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-03-16 23:19:11 +0530

    sign in and sign up page completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("");
   return (
     <div className="navbar">
@@ -44,7 +44,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
